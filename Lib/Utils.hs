f4ee3a74a6b14a62da5fe18ddf9bbeeb513c8bdb
Sguazz <p.sguazz@gmail.com>
2015-07-17 16:26:37 +0200

    Safe maps
    After all, should be able to ask bad values to these maps, too.

--- Lib/Utils.hs
+++ Lib/Utils.hs
@@ -1,5 +1,7 @@
 module Lib.Utils where
 
+import Data.Maybe
+
 -- Defining a couple of useful things for later
 
 type MarkedList a = [(a, Bool)]
@@ -17,10 +19,15 @@ marks as bs = map (`elem` bs) as
 markList :: Eq a => [a] -> [a] -> MarkedList a
 markList as bs = zip as (marks as bs)
 
--- Pretend we have maps and we never ask for bad keys
+-- Poor man's maps
+
+safeVal :: Eq k => k -> Map k v -> Maybe v
+safeVal k = valueOf . filter ((== k) . fst)
+  where valueOf ((k,v):_) = Just v
+        valueOf []        = Nothing
 
 val :: Eq k => k -> Map k v -> v
-val k = snd . head . filter ((== k) . fst)
+val k = fromJust . safeVal k
 
 dropKeys :: Eq k => k -> Map k v -> Map k v
 dropKeys k = dropWhile ((/= k) . fst)
